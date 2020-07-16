258b561d11e7a8b2e6bfaf986c0f985af5145a96
Tarun Tomar <taruntomar022@icloud.com>
2020-07-16 20:51:41 +0530

    Time Conversion

--- timeConversion.java
+++ timeConversion.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main{
 
-public static void main(String[] args) {
+public static void main(String[] args) { 
 
 // write your code here.
     Scanner scn = new Scanner(System.in);
